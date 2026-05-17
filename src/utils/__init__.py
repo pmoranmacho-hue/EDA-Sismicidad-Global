bb2883277ec66ddf0a81b3f978f4a9c6c338e2d9
Abem-ds <abemdatascience@gmail.com>
2026-05-17 19:49:49 +0200

    Add files via upload
    Inicial utils como paquete de Python

---
+++ src/utils/__init__.py
@@ -0,0 +1,6 @@
+#Este archivo convierte la carpeta utils/ en un paquete Python.
+# Puedes importar las siguientes funciones:
+#
+#   from src.utils.visualizacion import pinta_distribucion_categoricas
+#   from src.utils.limpieza import tipificar_variables
+#   from src.utils.estadisticos import resumen_estadisticos
